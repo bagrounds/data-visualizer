a2224b272d8c4f9c10e2ff7e569c8dada9b95179
bagrounds <bagrounds@gmail.com>
2016-06-21 13:25:35 -0700

    [CHANGE] main file to initial skeleton

---
+++ data-visualizer.js
@@ -0,0 +1,43 @@
+/**
+ * Visualize data 
+ *
+ * @module data-visualizer
+ */
+;(function () {
+  'use strict'
+
+  /* imports */
+  var typeCheck = require('type-check').typeCheck
+  var barChart = require('lib/bar-chart')
+
+  /* exports */
+  module.exports = visualize
+
+  /**
+   *
+   * @function visualize
+   *
+   * @param {Object} options all function parameters
+   * @param {Function} callback handle results
+   */
+  function visualize (options, callback) {
+    
+  }
+
+  /**
+   * Validate options
+   *
+   * @param {Object} options that were passed to visualize
+   * @return {Error} if options are invalid
+   */
+  function invalidOptions (options) {
+    if (!typeCheck('Object',options)) {
+      var message = 'options should be an object'
+      return new Error(message)
+    }
+
+    return null
+  }
+
+
+})()
